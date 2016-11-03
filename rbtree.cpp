5e355261449f941245dc9850eeb0b9b1c8fa6e61
Henrik Schulz <Henrikschulz@hotmail.com>
2016-11-03 15:30:21 +0100

    startup

---
+++ rbtree.cpp
@@ -0,0 +1,12 @@
+namespace DM852{
+	template<typename Key, typename Value, typename compare = std::less<Key>>
+	class RBtree
+	{
+
+	public:
+		RBtree(arguments);
+		~RBtree();
+
+		/* data */
+	};
+}
